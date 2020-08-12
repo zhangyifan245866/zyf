d414204ea8a7ea8e9c26e129b8dbfa619cb33456
zhangyifan <2350109737@qq.com>
2020-08-12 23:16:00 +0800

    张义凡2020年8月12日23:15:52提交8.12作业

---
+++ 张义凡8.12作业/ssm/src/main/java/com/xiaoshu/entity/GoodsVo.java
@@ -0,0 +1,15 @@
+package com.xiaoshu.entity;
+
+public class GoodsVo extends Goods {
+
+	private String gname;
+
+	public String getGname() {
+		return gname;
+	}
+
+	public void setGname(String gname) {
+		this.gname = gname;
+	}
+	
+}
